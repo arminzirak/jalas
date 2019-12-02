e4ccb8f3b2bbb2431266f1f8efafff397ff51bc1
K4mran <kamran2727@gmail.com>
2019-12-02 23:32:52 +0330

    update room_server

--- jalas_app/rooms_server.py
+++ jalas_app/rooms_server.py
@@ -1,41 +1,49 @@
-import json
-
 import requests
 from requests.exceptions import HTTPError
 
 
 def get_rooms(start, end, timeout=3):
     uri = f'http://213.233.176.40/available_rooms?start={start}&end={end}'
+
     try:
         response = requests.get(uri, timeout=timeout)
         response.raise_for_status()
+
     except HTTPError as http_err:
         return f'HTTP error occurred: {http_err}'  # Python 3.6
     except Exception as err:
         return f'Other error occurred: {err}'  # Python 3.6
-
-    else: # TODO: is it necessary to put else here?
-        rooms = json.loads(response.content.decode('utf-8').replace("'", '"'))['availableRooms']
-        return rooms
+    else:
+        return response.content
 
 
-res = get_rooms("2017-09-13T19:00:00", "2020-09-13T20:00:00", 5)
-print(res)
+# res = get_rooms("2017-09-13T19:00:00", "2020-09-13T20:00:00", 5)
+# print(res)
 
 
 # TODO: remove these examples
 
 
 def reserve_room(room_number, username, start, end):
-    uri = f'http://213.233.176.40/rooms/{room_number}/reserve'
-    body = {
+    import requests
+
+    url = "http://213.233.176.40/rooms/801/reserve"
+
+    payload = "{\n        \"username\": \"rkhosravi\",\n        \"start\": \"2019-09-13T19:00:00\",\n        \"end\": \"2019-09-13T20:00:00\"\n\t\n}"
+    payload = {
         "username": username,
         "start": start,
-        "end": end
+        "end": end,
     }
-    print(start, end)
+    payload = str(payload)
+    headers = {
+    'Content-Type': "application/json",
+    'cache-control': "no-cache",
+    'Postman-Token': "eda3ddf2-adee-4304-9b70-5989ba41ad2a"
+    }
     try:
-        response = requests.post(uri, body)
+        response = requests.request("POST", url, data=payload, headers=headers)
+        print (response.text)
         response.raise_for_status()
 
     except HTTPError as http_err:
@@ -46,5 +54,6 @@ def reserve_room(room_number, username, start, end):
         return response.content
 
 
-res = reserve_room(801, "rkhosravi", "2019-09-13T19:00:00", "2019-09-13T20:00:00")
+
+res = reserve_room("801", "rkhosravi", "2019-09-13T19:00:00", "2019-09-13T20:00:00")
 print(res)
