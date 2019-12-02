40d7f9bd56c7f1fb87e0ee69dd3241d52539df32
armin <armin.zirak@tap30co.com>
2019-12-02 18:16:38 +0330

    time fields

--- jalas_app/models.py
+++ jalas_app/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 
@@ -7,6 +9,9 @@ class Meeting(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
 
+    start_date = models.DateTimeField(default=datetime.datetime.now())
+    end_date = models.DateTimeField(default=datetime.datetime.now())
+
     class Meta:
         ordering = ['created']
 
@@ -15,3 +20,16 @@ class Poll(models.Model):
     meeting = models.ForeignKey(Meeting, related_name='polls', on_delete=models.CASCADE, null=True)
     votes_agree = models.IntegerField(default=0)
     votes_disagree = models.IntegerField(default=0)
+
+    # TODO: use HoldTime
+    start_date = models.DateTimeField(default=datetime.datetime.now())
+    end_date = models.DateTimeField(default=datetime.datetime.now())
+
+
+class HoldTime(object):
+    def ___init__(self, start_date, end_date):
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def __repr__(self):
+        return self.start_date + '|' + self.end_date
