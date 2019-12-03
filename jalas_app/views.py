345e5967a0eecbc8e3685c18b9b68ace643b1113
armin <armin.zirak@tap30co.com>
2019-12-03 22:54:46 +0330

    structure refactor

--- jalas_app/views.py
+++ jalas_app/views.py
@@ -1,6 +1,5 @@
 import datetime
 import json
-import threading
 
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
@@ -46,25 +45,13 @@ def reserve_room(request):
     meeting = Meeting.objects.get(id=poll.meeting.id)
     start_date = str(poll.start_date).replace(" ", "T")[:-6]
     end_date = str(poll.end_date).replace(" ", "T")[:-6]
-    result, status_code = server_reserve_room(room_number, USERNAME, start_date, end_date)
-    if status_code == 200:
-        notify_meeting_owner(USER_GMAIL)
-        meeting.status = models.MeetingStatus.finalized.value
-        with open(MEETING_LOG_ADDRESS, "a") as f:
-            f.write("meeting_finalized,{},{}\n".format(meeting.id, 'ok'))
-    elif status_code == 400:
-        with open(MEETING_LOG_ADDRESS, "a") as f:
-            f.write("meeting_cancelled,{},{}\n".format(meeting.id, 'room_already_reserved'))
-    else:
+    result, status_code = reserve_room_service(meeting, room_number, start_date, end_date)
+    if status_code != 400:
         status_code = 200
-        meeting.status = models.MeetingStatus.pending.value
-        threading.Thread(target=reserve_retry(meeting, room_number, start_date, end_date)).start()
-        result = "{\"message\": \"reservation is pending\"}"
-    meeting.save()
+
     end_time = datetime.datetime.now()
     with open(TIMING_LOG_ADDRESS, "a") as f:
-        f.write(
-            "meeting_processed,{},{},{},{}\n".format(meeting.id, start_date, end_date, (end_time - start_time).seconds))
+        f.write("meeting_processed,{},{},{},{}\n".format(meeting.id, start_date, end_date, (end_time - start_time).seconds))
     return HttpResponse(result, status=status_code)
 
 
@@ -81,22 +68,38 @@ def cancel_meeting(request):
 def reserve_retry(meeting, room_number, start_date, end_date):
     print('started retrying...')
     for i in range(3):
-        print('attempt {}'.format(i + 1))
-        result, status_code = server_reserve_room(room_number, USERNAME, start_date, end_date)
+        print('attempt :{}'.format(i))
+        result, status_code = reserve_room_service(meeting, room_number, start_date, end_date, attempt=i + 1)
         if status_code == 200:
-            with open(MEETING_LOG_ADDRESS, "a") as f:
-                f.write("meeting_finalized,{},{}\n".format(meeting.id, 'ok'))
-            print('reserved in {} attempt'.format(i + 1))
-            notify_meeting_owner("jalas.445317@gmail.com")
-            meeting.status = models.MeetingStatus.finalized.value
             return
-        elif status_code == 400:
+
+
+def reserve_room_service(meeting, room_number, start_date, end_date, attempt=0):
+    result, status_code = server_reserve_room(room_number, USERNAME, start_date, end_date)
+    if status_code == 200:
+        with open(MEETING_LOG_ADDRESS, "a") as f:
+            f.write("meeting_finalized,{},{}\n".format(meeting.id, 'ok'))
+        print('reserved in {} attempt'.format(i + 1))
+        notify_meeting_owner(USER_GMAIL)
+        meeting.status = models.MeetingStatus.finalized.value
+        meeting.save()
+        return result, status_code
+    elif status_code == 400:
+        with open(MEETING_LOG_ADDRESS, "a") as f:
+            f.write("meeting_cancelled,{},{}\n".format(meeting.id, 'room_already_reserved'))
+        print('found room already reserved in {} attempt'.format(attempt + 1))
+        meeting.status = models.MeetingStatus.errored.value
+        meeting.save()
+        return result, status_code
+    else:
+        if attempt >= 3:
             with open(MEETING_LOG_ADDRESS, "a") as f:
-                f.write("meeting_cancelled,{},{}\n".format(meeting.id, 'room_already_reserved'))
-            print('found room already reserved in {} attempt'.format(i + 1))
+                f.write("meeting_cancelled,{},{}\n".format(meeting.id, 'sever_unavailable'))
+            print('attempts ended up failed')
             meeting.status = models.MeetingStatus.errored.value
-            return
-    with open(MEETING_LOG_ADDRESS, "a") as f:
-        f.write("meeting_cancelled,{},{}\n".format(meeting.id, 'sever_unavailable'))
-    print('attempts ended up failed')
-    meeting.status = models.MeetingStatus.errored.value
+            meeting.save()
+            return result, status_code
+        else:
+            reserve_room_service(meeting, room_number, start_date, end_date, attempt + 1)
+            result = "{\"message\": \"reservation is pending\"}"
+            return result, status_code
